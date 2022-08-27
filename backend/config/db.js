30236b19b3c1d351d3c2207c072858cb9f647ee9
abbos0959 <abbosakromovich777@gmail.com>
2022-08-27 08:37:00 -0700

    mern

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+   try {
+      const conn = await mongoose.connect(
+         `mongodb+srv://Abbos:teDzzxP6Q730IalD@cluster0.icji5b5.mongodb.net/ECommerUd?retryWrites=true&w=majority`
+      );
+
+      console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+   } catch (error) {
+      console.error(`Error: ${error.message}`.red.underline.bold);
+      process.exit(1);
+   }
+};
+
+export default connectDB;
