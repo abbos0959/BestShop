b47452386ad1ec7d2095270f26e493cd29da0eef
abbos0959 <abbosakromovich777@gmail.com>
2022-08-27 11:53:26 -0700

    make is better

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import colors from "colors";
 import morgan from "morgan";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
+const __dirname = path.resolve();
 
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -35,7 +36,6 @@ app.get("/api/config/paypal", (req, res) =>
    res.send("ARH781vMQYnRKV0c12rPraNXvLTYYJ9kB1J7eSxw4dfMpATWUSQYrB2xrILLxC7M-q6FB931nV0K8utW")
 );
 
-const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
@@ -54,6 +54,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
+console.log(process.env.PORT);
 
 app.listen(
    PORT,
